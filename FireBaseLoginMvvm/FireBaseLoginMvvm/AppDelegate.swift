f38f53be85dcbb1d3076a46519e03f0b22479f55
Lauriane75 <lauriane.haydari@gmail.com>
2020-11-29 10:51:07 +0100

    Create initial project

---
+++ FireBaseLoginMvvm/FireBaseLoginMvvm/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  FireBaseLoginMvvm
+//
+//  Created by Lauriane Haydari on 29/11/2020.
+//  Copyright © 2020 Lauriane Haydari. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ FireBaseLoginMvvm/FireBaseLoginMvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBaseLoginMvvm
+//
+//  Created by Lauriane Haydari on 29/11/2020.
+//  Copyright © 2020 Lauriane Haydari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
